009d550fff8a886ad335a48348d4026b9b2d2d35
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-05-14 16:49:03 +0400

    Adding data in all tables

--- HotelDatabase.sql
+++ HotelDatabase.sql
@@ -63,3 +63,75 @@ C_ID int NOT NULL, foreign key (C_ID) references Customer(C_ID),
 Booking_ID int NOT NULL,foreign key (Booking_ID) references Booking(Booking_ID),
 
 );
+
+
+-- use database
+use hotel;
+-------------- adding data into tables ------------------------
+
+SELECT * FROM Staff;
+INSERT INTO Staff ( S_Name , Job_Title , Salary)
+VALUES
+('Fatima Al-Mahrouqi', 'Branch Manager', 1200.00),
+('Hassan Al-Habsi', 'Accountant', 950.00),
+('Salma Al-Kalbani', 'Receptionist', 600.00),
+('Majid Al-Balushi', 'Loan Officer', 1000.00),
+('Alya Al-Siyabi', 'IT Support', 850.00);
+
+-------------------------------------
+SELECT * FROM Branch;
+INSERT INTO Branch ( B_Name , Location , S_ID)
+VALUES 
+('Central Branch', 'Muscat', 1),
+('North Branch', 'Sohar', 2),
+('East Branch', 'Sur', 3),
+('South Branch', 'Salalah', 4),
+('West Branch', 'Nizwa', 5);
+
+---------------------------------
+
+SELECT * FROM Customer;
+INSERT INTO Customer ( C_Name ,Email )
+VALUES 
+('Ahmed Al-Mamari', 'ahmedmamari@gmail.com'),
+('Salma Al-Busaidi', 'salmabusaidi@gmail.com'),
+('Yousef Al-Lawati', 'youseflawati@gmail.com'),
+('Alya Al-Harthy', 'alyaharthy@gmail.com'),
+('Khalid Al-Shukaili', 'khalidshukaili@gmail.com');
+
+--------------------------------------
+
+SELECT * FROM CustomersPhone;
+INSERT INTO CustomersPhone(C_ID, Phone)
+VALUES 
+(1, '97865432'),
+(2, '92345678'),
+(3, '90123456'),
+(4, '98765432'),
+(5, '93456789');
+
+----------------------------
+
+SELECT * FROM Booking;
+
+INSERT INTO Booking ( Check_In_Date , Check_Out_Date , C_ID , S_ID)
+VALUES
+('2025-05-15', '2025-05-18', 1, 2),
+('2025-06-01', '2025-06-05', 2, 3),
+('2025-06-10', '2025-06-12', 3, 4),
+('2025-07-01', '2025-07-10', 4, 5),
+('2025-07-15', '2025-07-20', 5, 1);
+
+------------------------------
+
+SELECT * FROM Room;
+INSERT INTO Room ( R_Type , Nightly_rate , CheckAvailability , Branch_ID , C_ID , Booking_ID)
+VALUES 
+('Single', 1, 'Available', 1, 1, 1),
+('Double', 2, 'NotAvailable',    2, 2, 2),
+('Suite',  4, 'Available', 3, 1, 1),
+('Single', 5, 'NotAvailable',    4, 3, 3);
+
+ALTER TABLE Room
+alter COLUMN CheckAvailability nvarchar(60);
+
